5c5eb6924e686b444df1f5fa8fb5b13fd98e7133
gboigwe <geakande@gmail.com>
2024-01-03 12:30:35 -0800

    print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    if i < 10:
-        print("{}{}".format(0, i), end=charend)
-    else:
-        if i == 99:
-            charend = "\n"
-        print(i, end=charend)
+
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    digit2 = i % 10
-    digit1 = i / 10
-    if i < 10 and digit1 < digit2:
-        print("{}{}".format(0, i), end=charend)
-    elif digit1 < digit2:
-        if i == 89:
-            charend = "\n"
-        print(i, end=charend)
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:02d}, ".format(i * 10 + j), end="")
+        else:
+            print("{:02d}".format(i * 10 + j))
