3bfded2e76dbd697a6c8bc438c8ae4d1571484cb
gboigwe <geakande@gmail.com>
2024-01-03 12:38:57 -0800

    digit add

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
-    if number < 0:
-        ldigit = -number % 10
-        ldigit = ldigit
-    else:
-        ldigit = number % 10
-    print(ldigit, end="")
-    return (ldigit)
+    last_digit = number % 10
+    print(last_digit, end="")
+    return last_digit
