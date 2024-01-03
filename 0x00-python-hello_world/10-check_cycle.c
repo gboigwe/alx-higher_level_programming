eab6ecd1e927546e9663a2380a56db557d872b13
gboigwe <geakande@gmail.com>
2024-01-03 09:58:26 -0800

    c code check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
- * check_cycle - Check if a linked list has a cycle in it.
- * @list: Value check.
- * Return: 0 if there is no cycle, 1 if there is a cycle.
-*/
+ * check_cycle - check list
+ * @list: list
+ * Return: 1 or  0.
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortuga = list;
-	listint_t *conejo = list;
+	listint_t *iter1;
+	listint_t *iter2;
 
-	while (conejo && conejo->next)
+	iter1 = list;
+	iter2 = list;
+	if (list == NULL)
 	{
-		tortuga = tortuga->next;
-		conejo = conejo->next->next;
-
-		if (tortuga == conejo)
-			return (1);
+		return (0);
 	}
-
-	return (0)
+	iter2 = iter2->next;
+	while (iter2 != NULL && iter2->next != NULL && iter1 != NULL)
+	{
+		if (iter1 == iter2)
+		{
+			return (1);
+		}
+		iter1 = iter1->next;
+		iter2 = iter2->next->next;
+	}
+	return (0);
 }
