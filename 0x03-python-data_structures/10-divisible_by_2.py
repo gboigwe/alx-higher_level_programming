7022a565118fac2193c17ac405b888ec553052e8
gboigwe <geakande@gmail.com>
2024-01-09 18:59:13 +0100

    fix no C

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mulby2 = []
+    new_list = []
     for i in my_list:
-        if (i % 2) == 0:
-            mulby2.append(True)
+        if i % 2 == 0:
+            new_list.append(True)
         else:
-            mulby2.append(False)
+            new_list.append(False)
 
-    return(mulby2)
+    return new_list
