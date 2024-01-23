02d8bf4f395851b5ea4fd536c1a6f456f3ca4e43
gboigwe <geakande@gmail.com>
2024-01-23 23:52:45 +0100

    square two

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creating a square class module in Python
+"""
+
+
+class Square:
+    """class defined for square generation
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
