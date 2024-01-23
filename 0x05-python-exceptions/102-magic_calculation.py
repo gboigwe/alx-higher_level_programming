bbedc70b6b4305fc0d744ffbe68c9a4c02a992b6
gboigwe <geakande@gmail.com>
2024-01-23 02:47:29 +0100

    Byte Code

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    ans = 0
+    for index in range(1, 3):
+        try:
+            if index > a:
+                raise Exception("Too far")
+            else:
+                ans += (a**b) / index
+        except:
+            ans = b + a
+            break
+        return result
