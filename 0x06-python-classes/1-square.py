48841e16045578e8e322811bd49bb943188af6f7
gboigwe <geakande@gmail.com>
2024-01-23 23:48:23 +0100

    square zero

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creating a square module for python
+"""
+
+
+class Square:
+    """creating and initializing a square
+    """
+    def __init__(self, size):
+        self.__size = size
