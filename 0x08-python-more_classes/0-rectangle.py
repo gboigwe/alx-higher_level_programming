3a8a8a03fa03cb99b46e8aa021f5ab6e018a0cf9
gboigwe <geakande@gmail.com>
2024-01-29 18:18:33 +0100

    zero

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a rectangle class
+"""
+
+
 class Rectangle:
+    """Class Rectangle: A rectangle class
+    __init__: initializes the class with private argument width and height
+    """
     pass
-
-
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
