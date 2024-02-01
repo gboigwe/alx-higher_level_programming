fd5b37a8e85522256b3cbdcf5d3402e9212a1512
gboigwe <geakande@gmail.com>
2024-02-01 12:11:10 +0100

    rectangle 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -78,29 +78,3 @@ class Rectangle:
             return rect_2
         else:
             return rect_1
-#        try:
-#            rect_1.area() >= rect_2.area()
-#        except:
-#            raise TypeError("rect_1 must be an instance of Rectangle")
-#        try:
-#            rect_2.area() >= rect_1.area()
-#        except:
-#            raise TypeError("rect_2 must be an instance of Rectangle")
-#        return rect_1.area()
-
-
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(2, 3)
-
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
-    
-
-my_rectangle_2.width = 10
-my_rectangle_2.height = 5
-if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
-    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
-else:
-    print("my_rectangle_2 is bigger than my_rectangle_1")
