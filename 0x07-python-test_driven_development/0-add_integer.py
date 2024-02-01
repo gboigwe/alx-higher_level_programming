7db5d4aaf75d95e64ad1d8824dea33b6b11464c1
gboigwe <geakande@gmail.com>
2024-02-01 18:33:49 +0100

    tdd add int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,3 +19,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile()
