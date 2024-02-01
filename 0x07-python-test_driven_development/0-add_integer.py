eaee271fb41703d1822e64e0ef40c59ac16db0bd
gboigwe <geakande@gmail.com>
2024-02-01 17:29:55 +0100

    tdd add int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""Defines arg integer function"""
+
+
 def add_integer(a, b=98):
+    """Returns the addition of variable a and b
+    Variables must be int and float
+    
+    Variables must be casted as an int
+
+    Returns a calculated integer
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
+    if isinstance(a, float) and isinstance(b, float):
         return int(a) + int(b)
