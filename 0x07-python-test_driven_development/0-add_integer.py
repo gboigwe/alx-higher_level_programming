3e382e31aafa510dac1bc21fb263deb827163a35
gboigwe <geakande@gmail.com>
2024-02-01 17:55:03 +0100

    tdd add int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     Raise:
         TypeError: if either a orb is not an integer or float
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
