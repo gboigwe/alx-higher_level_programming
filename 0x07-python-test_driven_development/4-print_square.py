4ee6833b014ceb77c0be0215e0b606bd3b7c0e5b
gboigwe <geakande@gmail.com>
2024-02-02 06:02:20 +0100

    tdd print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a function that prints Square module with "#".
+
+useful for all square-based applications
+"""
+
+
+def print_square(size):
+    """
+    using size of length of the square
+    size must be an integer
+    """
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for x in range(size):
+        print('#' * size)
