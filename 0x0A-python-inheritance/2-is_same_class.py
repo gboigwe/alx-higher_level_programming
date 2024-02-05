19f27145e85857d3d023f63a82bd0f46e4730544
gboigwe <geakande@gmail.com>
2024-02-05 17:39:19 +0100

    A function that checks the data type of an object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Defines a function that checks for data type"""
+
+
 def is_same_class(obj, a_class):
+    """A function that checks the data type of object
+    
+    Args:
+        obj: The object to be considered
+        
+        a_class: Datatype to be considered
+    
+    Return True if datatype match and false if it doesn't
+    """
     if type(a_class) is type(obj):
         return True
     return False
