91361bdef109f91adc78f5f02fd066d6fe15e694
gboigwe <geakande@gmail.com>
2024-02-05 22:54:15 +0100

    This code defines a funcion that is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defining an empty class"""
+
+
+class BaseGeometry:
+    """The class is empty and would pass afterwards"""
+
+    def __init__(self):
+        """Initializes an empty function"""
+
+        pass
+
+    def area(self):
+        """Defines a function for area of a geometry
+        This function is not defined and can't be used
+
+        It raises an error message to the developer to
+        define it before use
+        """
+
+        raise Exception ("area() is not implemented")
