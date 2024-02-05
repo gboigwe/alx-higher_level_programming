e6eaf65577eda207c48f0dc39016f3dd26755c5b
gboigwe <geakande@gmail.com>
2024-02-05 02:41:50 +0100

    Testing the print out of text string indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,6 +13,6 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for delims in ":.?":
-        text = (delims +
-                "\n\n").join([i.strip(" ") for i in text.split(delims)])
+        text = (delims + "\n\n").join(
+            [i.strip(" ") for i in text.split(delims)])
     print("{}".format(text), end="")
