95dc57aef6d2de35d304ede4de15114f522cfbbb
gboigwe <geakande@gmail.com>
2024-02-05 17:19:36 +0100

    This code sorts out a list and test all possibility

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,6 @@ class MyList(list):
     list as an object in class MyList
     """
 
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         s = sorted(list(self))
         print(s)
