433135a579519b7a1357ac76a7683d5779fc32a2
gboigwe <geakande@gmail.com>
2024-02-07 04:31:18 +0100

    class inheriting an integer validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -51,24 +51,26 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
+    """Defining a Rectangle class
+    Initializing and inheriting BaseGeometry class
+
+    Args:
+        width: The value of the width is passed in
+        height: The value of the height is passed in
+    """
+
     def __init__(self, width, height):
-        BaseGeometry.__init__(self)
+        """Initializaion is called on the class
+            width is initialized
+            height is initialized
+        """
+
+        super().__init__()
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def area(self):
+        """Defines an area for the rectangle"""
+        return self.__width * self.__height
