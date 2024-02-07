c20e6a7935eb7d57641201da5712984bffe642ee
gboigwe <geakande@gmail.com>
2024-02-07 19:01:09 +0100

    Loads a JSON String from a python object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Loads a JSON String from a python object."""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Loads a JSON String from a python object.
+
+    Args:
+        my_str: The String to be considered.
+        Must be a Python Object.
+
+    Returns:
+        objs: The loaded string.
+    """
+
+    objs = json.dumps(my_str)
+    return json.loads(objs)
