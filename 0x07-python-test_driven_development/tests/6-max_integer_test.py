309b9f1999f8f5efc7ea278bc0c4d828319f50be
gboigwe <geakande@gmail.com>
2024-02-08 14:50:50 +0100

    Implement type checks for max_integer & add test cases

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    """Suite test for max_integer function"""
+
+    def test_maximum_int(self):
+        self.assertEqual(max_integer([5, -2, 100, 3]), 100)
+
+    def test_nil_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_num_repeat(self):
+        self.assertEqual(max_integer([1000, 1000, 1000]), 1000)
+
+    def test_nums_decimal(self):
+        self.assertEqual(max_integer([50, 51, 50, 49]), 51)
+
+    def text_maximum_ope_int(self):
+        self.assertEqual(max_integer([-3, -5 * -5, 12, -1]), 25)
+
+    def test_thenegative_nums(self):
+        self.assertEqual(max_integer([-10, -5, -2, -1]), -1)
+
+    def test_begin_max(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
+    def test_nums_that_zero(self):
+        self.assertEqual(max_integer([0, 0]), 0)
+
+    def test_large_list(self):
+        self.assertEqual(max_integer([
+            901, 902, 903, 904, 905, 906, 907, 908, 909, 910,
+            911, 912, 913, 914, 915, 916, 917, 918, 919, 920,
+            919, 918, 917, 1000, 915, 914, 913, 912, 911, 910,
+            909, 908, 907, 906, 905, 904, 903, 902, 901]), 1000)
+
+    def testing_list_loop(self):
+        my_list = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer([i * 5 for i in my_list]), 25)
+
+    def test_one_number_in_a_list(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_string_number_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([0, '1'])
+
+    def test_tuple_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([10, (20, 30)])
+
+    def test_dictionary(self):
+        with self.assertRaises(KeyError):
+            max_integer({'key1': 1, 'key2': 2})
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(1)
