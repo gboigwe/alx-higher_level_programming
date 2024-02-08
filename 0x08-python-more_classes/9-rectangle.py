7cd8e5dd2cf66c0ef20313f31506fed9e519c4b3
gboigwe <geakande@gmail.com>
2024-02-08 12:02:28 +0100

    Improve square creation method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
     @classmethod
-    def square(cls, size=0):    
+    def square(cls, size=0):
         """Generates square instance with equal width and height.
         Args:
             cls: Access class attributes.
