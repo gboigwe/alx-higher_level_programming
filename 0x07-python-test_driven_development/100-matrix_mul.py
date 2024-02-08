3469c109a67d06bf2d7d1069fdee4f937837f200
gboigwe <geakande@gmail.com>
2024-02-08 16:19:37 +0100

    Refactor matrix_mul for clarity

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -33,12 +33,12 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    for elems in m_a:
-        if not isinstance(elems, list):
+    for element_item in m_a:
+        if not isinstance(element_item, list):
             raise TypeError("m_a must be a list of lists")
 
-    for elems in m_b:
-        if not isinstance(elems, list):
+    for element_item in m_b:
+        if not isinstance(element_item, list):
             raise TypeError("m_b must be a list of lists")
 
     if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
@@ -47,49 +47,49 @@ def matrix_mul(m_a, m_b):
     if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
         raise ValueError("m_b can't be empty")
 
-    for lists in m_a:
-        for elems in lists:
-            if not type(elems) in (int, float):
+    for list_type in m_a:
+        for element_item in list_type:
+            if not type(element_item) in (int, float):
                 raise TypeError("m_a should contain only integers or floats")
 
-    for lists in m_b:
-        for elems in lists:
-            if not type(elems) in (int, float):
+    for list_type in m_b:
+        for element_item in list_type:
+            if not type(element_item) in (int, float):
                 raise TypeError("m_b should contain only integers or floats")
 
-    length = 0
+    len_size = 0
 
-    for elems in m_a:
-        if length != 0 and length != len(elems):
+    for element_item in m_a:
+        if len_size != 0 and len_size != len(element_item):
             raise TypeError("each row of m_a must be of the same size")
-        length = len(elems)
+        len_size = len(element_item)
 
-    length = 0
+    len_size = 0
 
-    for elems in m_b:
-        if length != 0 and length != len(elems):
+    for element_item in m_b:
+        if len_size != 0 and len_size != len(element_item):
             raise TypeError("each row of m_b must be of the same size")
-        length = len(elems)
+        len_size = len(element_item)
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    r1 = []
-    i1 = 0
+    results_1 = []
+    index_item_1 = 0
 
     for a in m_a:
-        r2 = []
-        i2 = 0
+        results_2 = []
+        index_item_2 = 0
         num = 0
-        while (i2 < len(m_b[0])):
-            num += a[i1] * m_b[i1][i2]
-            if i1 == len(m_b) - 1:
-                i1 = 0
-                i2 += 1
-                r2.append(num)
+        while (index_item_2 < len(m_b[0])):
+            num += a[index_item_1] * m_b[index_item_1][index_item_2]
+            if index_item_1 == len(m_b) - 1:
+                index_item_1 = 0
+                index_item_2 += 1
+                results_2.append(num)
                 num = 0
             else:
-                i1 += 1
-        r1.append(r2)
+                index_item_1 += 1
+        results_1.append(results_2)
 
-    return r1
+    return results_1
