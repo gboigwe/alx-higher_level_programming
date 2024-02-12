b061d026c094c8f943a0913cdc55a3eb2b479847
gboigwe <geakande@gmail.com>
2024-02-12 18:02:37 +0100

    Updated Class to handle error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,47 +22,80 @@ class Rectangle(Base):
         """
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-
-    @property
-    def height(self):
-        """Getting the instance height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setting the instance height"""
-        self.__height = value
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-        """Getting the instance width"""
+        """Getting the private instance width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setting the instance width"""
+        """Setting the private instance width"""
+        if type(value) != int:
+            """Check for the data type"""
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            """Chcke if value is less than or equal to 0"""
+            raise ValueError("width must be > 0")
+        """Set the value"""
         self.__width = value
+        """Initialize with the id"""
+        super().__init__(id)
+
+    @property
+    def height(self):
+        """Getting the private instance height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting the private instance height"""
+        if type(value) != int:
+            """Check for the data type"""
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            """Check if value is less than or equal to 0"""
+            raise ValueError("height must be > 0")
+        self.__height = value
+        """Initialize with the id"""
+        super().__init__(id)
 
     @property
     def x(self):
-        """Getting the instance x"""
+        """Getting the private instance x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setting the instance x"""
+        """Setting the private instance x"""
+        if type(value) != int:
+            """Check for the data type"""
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            """Check if value is less than 0"""
+            raise ValueError("x must be >= 0")
         self.__x = value
+        """Initialize with the id"""
+        super().__init__(id)
 
     @property
     def y(self):
-        """Getting the instance y"""
+        """Getting the private instance y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setting the instance y"""
+        """Setting the private instance y"""
+        if not isinstance(value, int):
+            """Check for the data type"""
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            """Check if value is less than 0"""
+            raise ValueError("y must be >= 0")
         self.__y = value
+        """Initialize with the id"""
+        super().__init__(id)
