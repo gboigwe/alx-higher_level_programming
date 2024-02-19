6fd77e40d9fae1b2385c766334d492912c00ab57
gboigwe <geakande@gmail.com>
2024-02-19 15:07:14 +0100

    Updating pending updates

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import json
+def from_json_string(my_obj, filename):
+    with open(filename, mode="w", encoding="utf-8") as f:
+        ld = json.dump(my_obj, filename)
+        f.write(ld)
+        return json.load(ld)
+
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+s_my_dict = """
+{"is_active": true, "info": {"age": 36, "average": 3.14}, 
+"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+"""
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    s_my_dict = """
+    {"is_active": true, 12 }
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+
