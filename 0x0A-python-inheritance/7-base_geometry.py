e64174cb3440428169d64f9c8e9a16f1933ae2e3
gboigwe <geakande@gmail.com>
2024-02-21 00:57:45 +0100

    checks if isinstance class is inherited

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -42,7 +42,7 @@ class BaseGeometry:
         Raises error if value is <= 0
         """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
