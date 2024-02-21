733569f9e409266c48a1f836eefc470bd005acaf
gboigwe <geakande@gmail.com>
2024-02-21 12:09:00 +0100

    Added str() to return class Square description

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,13 +19,3 @@ def inherits_from(obj, a_class):
     if not issubclass(type(obj), a_class):
         return False
     return True
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
-
