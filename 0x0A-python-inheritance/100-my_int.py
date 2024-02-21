ad77fadb5a0acdf9ee0caaf7f5da36f21273df3a
gboigwe <geakande@gmail.com>
2024-02-21 12:43:03 +0100

    Wrote a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
+"""Creating a class that acts like int
+    Acts like but in an inverted way
+"""
 
 
 class MyInt(int):
+    """Created the class"""
+
     def __init__(self, value):
+        """Initializing the class
+        Args:
+            value : value is validated
+        """
+
         self.__value = value
         super().__init__()
-    
+
     def __eq__(self, other):
-        self.__other = other
-        return self.__value == self.__other
-    
+        """Defining a function that handles == operator
+        Args:
+            other : Other is inverted
+        Returns:
+            Returning the inverted value handled by eq function
+        """
+
+        return not super().__eq__(other)
+
     def __ne__(self, other):
-        return not self.__eq__(other)
+        """Defining a function that handles != operator
+        Args:
+            other : Other is inverted
+        Returns:
+            Returning the inverted value handled by ne function
+        """
+
+        return not super().__ne__(other)
 
 my_i = MyInt(3)
 print(my_i)
