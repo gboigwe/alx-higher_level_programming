364b7c211f373ffabe79f5a0569ffbc0131a61c5
gboigwe <geakande@gmail.com>
2024-03-13 17:18:39 +0100

    A script to insert into a row on a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
 -- SQL script that inserts a new row in table
 -- Insert a row to a table
-INSERT INTO first_table (
-	id 89,
-	name "Best School"
-);
+INSERT INTO first_table ( id, name ) VALUES ( 89, "Best School");
