b2d68db7600b4674c2220a329a89a25d65a31110
gboigwe <geakande@gmail.com>
2024-03-13 14:32:27 +0100

    Added Read Me

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,3 +10,15 @@ def lookup(obj):
 
     obj1 = dir(obj)
     return obj1
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,5 @@
+## SQL - Introduction
+
+Author:
+### Gbolahan Akande
+This project contains introduction to my sql
