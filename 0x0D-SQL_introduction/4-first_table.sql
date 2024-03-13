3baa784974ddae5bb5edf3f82e76c7684eed0ba1
gboigwe <geakande@gmail.com>
2024-03-13 16:28:27 +0100

    A script to create a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- SQL script to create a table
 -- Creating a table in the current directory
-CREATE first_table IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS first_table (
 	id = INT,
 	name = VARCHAR(256)
 );
