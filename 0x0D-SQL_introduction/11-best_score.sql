f5a3ea50a7e893c87a3e240b3f378a62ff31dfbe
gboigwe <geakande@gmail.com>
2024-03-14 02:24:49 +0100

    Script that list in an order in a particular range

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- SQL script that list all records based on a value of the table
+-- List all records of the table based o score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
