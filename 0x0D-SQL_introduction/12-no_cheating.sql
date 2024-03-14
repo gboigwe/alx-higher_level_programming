31cddedda66b58420c6eeffc2f247236c68d4991
gboigwe <geakande@gmail.com>
2024-03-14 02:36:21 +0100

    Script that updates a table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,3 @@
 -- SQL script that updates the records of score
 -- Update the records of score
-SELECT score FROM second_table WHERE name = "Bob";
-INSERT INTO second_table ( name, score )
-VALUES ( "Bob", 10 );
+UPDATE second_table SET score = 10 WHERE name = "Bob";
