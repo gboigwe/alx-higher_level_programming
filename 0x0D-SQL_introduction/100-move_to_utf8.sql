f671bb1be4e3a01b090dc4c5c564deaaf66b7afc
gboigwe <geakande@gmail.com>
2024-03-14 05:23:38 +0100

    script that converts database to UTF8 utf8mb4

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- SQL script that converts the daabase UTF8
+-- Convert database to UTF8
+-- ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4
+-- COLLATE = utf8mb4_unicode_ci
+-- ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb_unicode_ci
