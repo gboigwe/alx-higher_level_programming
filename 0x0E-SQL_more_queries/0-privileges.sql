961b1eb46f1e8b6f1d3f1b6671459e8ddb79b1dd
gboigwe <geakande@gmail.com>
2024-03-15 07:16:28 +0100

    SQL script to list all priviledged users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- SQL script to list all privileges for MySQL users
+-- Lists all privileges of users in user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
