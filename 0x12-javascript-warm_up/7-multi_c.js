a8c601d9ed3c02559fcf83a93ba65143f91aebf8
gboigwe <geakande@gmail.com>
2024-04-08 21:14:51 +0100

    Code that prints a string in number of args times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myVar = parseInt(process.argv[2])
+//let myWord = 'C is fun';
+//let i = 0;
+if (!isNaN(myVar) && Number.isInteger(myVar)) {
+	let myWord = 'C is fun';
+	let i = 0;
+	while (i < myVar) {
+		console.log(myWord);
+		i++;
+	}
+}
+else {
+	console.log('Missing number of occurences');
+}
