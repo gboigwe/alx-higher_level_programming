085f5ca78cd63043026a90bd24558634be9bc8ac
gboigwe <geakande@gmail.com>
2024-04-08 22:15:55 +0100

    Reprinting passed argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv[2];
-console.log(myVar ? myVar : 'No argument');
+if (myVar === undefined) {
+    console.log('No argument');
+} else {
+    console.log(myVar);
+}
