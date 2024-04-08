9b51ca8cb657a405289bb45116d0d1bde77ab01f
gboigwe <geakande@gmail.com>
2024-04-08 16:58:46 +0100

    Code that checks for argument variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const myVar = process.argv.length;
-console.log(myVar === 2 ? 'No argument' : myVar === 3 ? 'Argument found' : 'Arguments found');
-// if (myVar === 2) {
-// 	console.log('No argument');
-// } else if (myVar === 3) {
-// 	console.log('Argument found');
-// } else {
-// 	console.log('Argument found');
-// }
+if (myVar === 2) {
+	console.log('No argument');
+} else if (myVar === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
