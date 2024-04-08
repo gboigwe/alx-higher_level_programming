2e06f031ba73e276be88acbb4b371373c4046107
gboigwe <geakande@gmail.com>
2024-04-08 14:32:42 +0100

    Wrote my first javascript code

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
+
 // Created a constant variable
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Log in console my variable
 console.log(myVar);
