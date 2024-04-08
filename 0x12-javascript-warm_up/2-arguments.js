7d85c6c5a5ab251d49b333a5a1c300275919a468
gboigwe <geakande@gmail.com>
2024-04-08 15:25:47 +0100

    Code to check number of arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+	console.log('No argument');
+}
+else if (process.argv[2]) {
+	console.log('Argument found');
+}
+else {
+	console.log('Argument found');
+}
