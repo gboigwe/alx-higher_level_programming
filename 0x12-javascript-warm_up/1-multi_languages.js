0981bc24bedadafd478fd14d1b7447909f36bafd
gboigwe <geakande@gmail.com>
2024-04-08 15:54:12 +0100

    Code that uses console to print a variable

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log('"C is fun"');
-console.log('"Python is cool"');
-console.log('"JavaScript is amazing"');
+const myVar = '"C is fun"\n"Python is cool"\n"JavaScript is amazing"';
+console.log(myVar);
