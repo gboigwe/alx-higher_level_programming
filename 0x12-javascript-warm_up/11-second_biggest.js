01d9252ef3f7c8513b1964954fd1e5b16eb2e830
gboigwe <geakande@gmail.com>
2024-04-09 08:54:53 +0100

    Code that search for second big integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const sArg = process.argv.length;
 if (sArg <= 3) {
-    console.log(0);
-}
-else {
-    const theSlice = process.argv.slice(2, sArg)
+  console.log(0);
+} else {
+  const theSlice = process.argv.slice(2, sArg)
     .map(Number).sort((a, b) => a - b);
-    console.log(theSlice[theSlice.length - 2]);
+  console.log(theSlice[theSlice.length - 2]);
 }
