5751479c40d3fe986db8d22ca656a2ee1384c696
gboigwe <geakande@gmail.com>
2024-04-09 06:10:15 +0100

    Reprinting passed in argument

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (!isNaN(size) && Number.isInteger(size)) {
-    for (let i = 0; i < size; i++) {
-        myVar = '';
-        for (let j = 0; j < size; j++) myVar += 'X'
-            console.log(myVar);
-    }
-}
-else {
-    console.log("Missing size");
+  for (let i = 0; i < size; i++) {
+    let myVar = '';
+    for (let j = 0; j < size; j++) myVar += 'X';
+    console.log(myVar);
+  }
+} else {
+  console.log('Missing size');
 }
