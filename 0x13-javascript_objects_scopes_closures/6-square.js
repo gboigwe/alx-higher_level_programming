3c243aca68dd92e2eb728d6ca5ee870d86e2aaf4
gboigwe <geakande@gmail.com>
2024-04-10 02:31:08 +0100

    class Square that inherits from Rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
 
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
+        let i = 0;
+        while (i < this.height) {
+          let row = '';
+          let j = 0;
+          while (j < this.width) {
             row += c;
+            j++;
+          }
+          console.log(row);
+          i++;
         }
-        console.log(row);
-      }
     }
   }
 };
