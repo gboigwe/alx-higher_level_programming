a37f033756c27ae23e2e8624b0c8c5456ce399af
gboigwe <geakande@gmail.com>
2024-04-14 19:26:48 +0100

    Script that takes in an argument and displays by it

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,11 +22,13 @@ if __name__ == '__main__':
     )
 
     cursor = conn_db.cursor()
+
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE {}%".format(forth_arg)
+        "SELECT * FROM states WHERE name LIKE '%{}%'".format(forth_arg)
         )
 
     rows = cursor.fetchall()
+
     for row in rows:
         print(row)
 
