a120c84344a873046dc881f0e748fd5fd1017fd4
gboigwe <geakande@gmail.com>
2024-04-15 23:00:20 +0100

    a python file that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,17 +8,8 @@ of a State and an instance Base = declarative_base()
 from sys import argv
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
-
-
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'
-    .format(argv[1],
-            argv[2],
-            argv[3]), pool_pre_ping=True
-            )
-Base.metadata.create_all(engine)
-
 
 class State(Base):
     """
