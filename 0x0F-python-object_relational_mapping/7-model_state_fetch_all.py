1968e19f1bb17d5f8e6708e66313a5b283ffbdbd
gboigwe <geakande@gmail.com>
2024-04-15 23:35:06 +0100

    a python file that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+A python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+Base = declarative_base()
+
+
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'
+    .format(argv[1],
+            argv[2],
+            argv[3]), pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(50))
+
+Session = sessionmaker(bind=engine)
+
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
