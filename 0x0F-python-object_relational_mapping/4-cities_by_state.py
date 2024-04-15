0b05fc785cb65eb29ec12d4f9d5bd44fac644472
gboigwe <geakande@gmail.com>
2024-04-15 14:06:25 +0100

    Updated the code to avoid errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,16 +6,16 @@ from the database hbtn_0e_4_usa
 
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
 
     conn_db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = conn_db.cursor()
