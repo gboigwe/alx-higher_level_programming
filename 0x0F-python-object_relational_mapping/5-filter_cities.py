57ee2433a0db8e7b0890978e2b5f62283a4866ba
gboigwe <geakande@gmail.com>
2024-04-15 15:41:58 +0100

    Script with an arg then lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,8 @@ using the database hbtn_0e_4_usa
 """
 
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == '__main__':
 
@@ -21,12 +21,18 @@ if __name__ == '__main__':
     )
 
     cursor = conn_db.cursor()
+    forth_arg = sys.argv[4]
 
-    cursor.execute()
+    query_l = "SELECT c.id, c.name, s.name \
+          FROM states s, cities c \
+          WHERE c.state_id = s.id \
+          ORDER BY c.id ASC"
+
+    cursor.execute(query_l, (forth_arg,))
 
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    
+
     cursor.close()
     conn_db.close()
