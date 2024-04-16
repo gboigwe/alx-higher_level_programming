28bb557cb62aa5170dafad198b8dcd7cc87478db
gboigwe <geakande@gmail.com>
2024-04-16 10:49:58 +0100

    a python file that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ of a State and an instance Base = declarative_base()
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -17,8 +17,6 @@ if __name__ == "__main__":
                 argv[2],
                 argv[3]), pool_pre_ping=True
         )
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
