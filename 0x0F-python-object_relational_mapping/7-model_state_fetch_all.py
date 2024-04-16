54ea003fe096df68093f6bf769e4e3ca272f864c
gboigwe <geakande@gmail.com>
2024-04-16 11:57:56 +0100

    a python file that contains the class def of a state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
                 argv[3]), pool_pre_ping=True
         )
 
+
     class State(Base):
         """Created a TABLE using ORM
         Object Relational Mapping
