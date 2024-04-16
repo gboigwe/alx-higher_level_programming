67c6ad0e73a45c1b53b782d769a07a477f8b2088
gboigwe <geakande@gmail.com>
2024-04-16 11:56:06 +0100

    a python file that contains the class definition of a State

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,20 +7,33 @@ of a State and an instance Base = declarative_base()
 
 from sys import argv
 from sqlalchemy import Column, String, Integer, create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
-Base = declarative_base()
 
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1],
+                argv[2],
+                argv[3]), pool_pre_ping=True
+        )
 
-class State(Base):
-    """Created a TABLE using ORM
-    Object Relational Mapping
-    """
+    class State(Base):
+        """Created a TABLE using ORM
+        Object Relational Mapping
+        """
 
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(128), nullable=False)
 
-    def __repr__(self):
-        return f"State(id={self.id}, name='{self.name}')"
+    Base = declarative_base()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
