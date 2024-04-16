35587af6a38c2bc3abec85a649e914b202855490
gboigwe <geakande@gmail.com>
2024-04-16 15:48:16 +0100

    script that prints first State object

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,14 +12,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]), pool_pre_ping=True)
-    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3], pool_pre_ping=True))
+
     session = Session(engine)
 
     for state in session.query(State).filter(State.name.like("%a%")):
         print(state.id, state.name, sep=": ")
     session.close()
-
