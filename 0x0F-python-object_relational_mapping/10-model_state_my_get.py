a3f1a9598f8cc6a3efd6fdf0dc9dda86218c0750
gboigwe <geakande@gmail.com>
2024-04-16 16:11:02 +0100

    script prints the State object with the name passed

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+"""
+Write a script that prints
+the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(
+                               argv[1],
+                               argv[2],
+                               argv[3], pool_pre_ping = True))
+
+    Session = sessionmaker(engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+
+    arg_forth = argv[4]
+
+    search = session.query(State).filter(State.name.like(arg_forth)).all()
+
+    if len(search) == 0:
+        print("Not Found")
+    else:
+        print(search[0].id)
+
+    session.close
