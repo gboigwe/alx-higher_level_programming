77425622a98231612fac255e3b54800740c897db
gboigwe <geakande@gmail.com>
2024-04-16 12:16:36 +0100

    a python file that contains the class defini of a state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         .format(argv[1],
                 argv[2],
                 argv[3]), pool_pre_ping=True
-        )
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
