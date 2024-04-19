faba34d7563e338495026ab54ddd56fed45ca90e
gboigwe <geakande@gmail.com>
2024-04-19 18:05:07 +0100

    Script that prints the State object with the name passed

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,14 +14,12 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(
-                               argv[1],
-                               argv[2],
-                               argv[3], pool_pre_ping = True))
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3], pool_pre_ping=True))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
-
 
     session = Session()
     arg_forth = argv[4]
