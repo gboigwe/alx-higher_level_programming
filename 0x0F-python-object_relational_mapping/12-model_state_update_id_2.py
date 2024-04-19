277810e0c72db7cda863035bbacf551acf52f98e
gboigwe <geakande@gmail.com>
2024-04-19 22:44:56 +0100

    Script that changes the name of a State object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,7 +14,7 @@ from sqlalchemy import asc
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           (argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     start = sessionmaker()
     start.configure(bind=engine)
