94c8bf7d878fe82cc52e8fde43e0e2f4ccdd1a01
gboigwe <geakande@gmail.com>
2024-04-25 17:11:04 +0100

    Script to take URL, send GET request and display the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes URL, sends a GET request to URL, and displays the body response
-curl -s -X GET "$1" -o /dev/null -w "%{http_code}" | grep -q 200 && curl -s "$1"
+curl -sX GET "$1" -L 200
