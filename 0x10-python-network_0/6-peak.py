5f55790c2c91b80435f69081fecd606dc11639bf
gboigwe <geakande@gmail.com>
2024-04-26 00:38:47 +0100

    A Func that finds a peak in a list of integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,3 +18,26 @@ def find_peak(list_of_integers):
         if num > list_peak:
             list_peak = num
     return list_peak
+
+
+
+def find_peak(list_of_integers):
+    """
+        A function that finds peak in a list.
+            def find_peak(list_of_integers):
+    """
+    if not list_of_integers:
+        return None
+
+    least = 0
+    peak = len(list_of_integers) - 1
+
+    while least < peak:
+        temp = (least + peak) // 2
+
+        if list_of_integers[temp] < list_of_integers[temp + 1]:
+            least = temp + 1
+        else:
+            peak = temp
+
+    return list_of_integers[least]
