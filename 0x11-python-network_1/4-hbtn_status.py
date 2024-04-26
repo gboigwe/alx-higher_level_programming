a6832463b0db3d903e0152675b91162646851efe
gboigwe <geakande@gmail.com>
2024-04-26 15:26:03 +0100

    Python script that fetches url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
+    Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 
 import requests
 
 
-r = requests.get("https://alx-intranet.hbtn.io/status")
-print("Body response:")
-print("\t- type: {}".format(type(r.text)))
-print("\t- content: {}".format(r.text))
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
