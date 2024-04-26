2ad41079f2d9417ddd080edce34d4a015d92a046
gboigwe <geakande@gmail.com>
2024-04-26 14:39:19 +0100

    Handling error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL,
+    sends a request to the URL and displays the
+    body of the response (decoded in utf-8).
+"""
+
+
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            html = res.read()
+            print(html.decode('uf-8'))
+    except urllib.error.HTTPError as e:
+        print("Enter code: {}".format(e.code))
