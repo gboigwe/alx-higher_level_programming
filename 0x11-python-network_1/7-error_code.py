f428f0cd8cd1318df452c077fc8bdcb5924f21f8
gboigwe <geakande@gmail.com>
2024-04-26 18:50:01 +0100

    Python script sends request to URL and display response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+"""
+import sys
+import requests
 
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.content.decode("utf-8")
