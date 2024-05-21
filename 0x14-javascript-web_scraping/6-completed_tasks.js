613265c9df130177bd546be6a0f84e18031adeb8
gboigwe <geakande@gmail.com>
2024-05-21 18:37:34 +0100

    code computes the number of task done by user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length <= 2) {
+  console.log('Usage: ./6-completed_tasks.js API_URL');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode === 200) {
+    const tasks = JSON.parse(body);
+    const completedTasks = tasks.filter(task => task.completed);
+    const userCompletedTasks = completedTasks.reduce((userTasks, task) => {
+      const userId = task.userId;
+      if (userTasks[userId]) {
+        userTasks[userId]++;
+      } else {
+        userTasks[userId] = 1;
+      }
+      return userTasks;
+    }, {});
+
+    for (const userId in userCompletedTasks) {
+      console.log(`${userId}: ${userCompletedTasks[userId]}`);
+    }
+  } else {
+    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+    process.exit(1);
+  }
+});
