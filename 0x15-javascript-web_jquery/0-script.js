f06cb697d9d9c7d3fce4ae7153d3fc2fb153973d
gboigwe <geakande@gmail.com>
2024-05-29 19:00:20 +0100

    JavaScript that updates the text color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Varaible header element using the DOM querySelector
+const header = document.querySelector('header');
+
+// Updating the color of the text to red #FF0000
+header.style.color = '#FF0000';
