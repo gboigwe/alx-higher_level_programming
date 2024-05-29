27cb246aa20254ff90f0f35c0a37e04b1526c882
gboigwe <geakande@gmail.com>
2024-05-29 20:01:38 +0100

    JavaScript that updates the text color

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Select the div with id 'red_header'
+  $('#red_header').click(function () {
+    // Select the header element using jQuery
+    $('header').addClass('red');
+  });
+});
